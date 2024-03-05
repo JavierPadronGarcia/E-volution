05a1cb8cdcb710c2a859ce878688d9f516a2d62e
Javier <padrongarciajavier04@gmail.com>
2024-03-05 19:50:04 +0000

    add logo structure and necessary fonts

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -52,7 +52,12 @@ export default function Login({
   };
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md gap-2">
+      <h1 className="text-center flex justify-center items-center">
+        <span className="font-poly italic text-2xl">E</span>
+        <span className="flex align-middle">-volution</span>
+      </h1>
+
       <Link
         href="/"
         className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
