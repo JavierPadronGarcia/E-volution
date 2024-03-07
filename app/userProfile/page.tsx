4b4f6402bad45f4a8f6bb3d382db43be647c6da5
svanlaugbirna <svanlaugbirna@gmail.com>
2024-03-07 11:42:53 +0000

    placed the burgermenu and arrow components in the right places

---
+++ app/userProfile/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BackArrow from "@/components/backArrow";
+import Hamburgermenu from '@/components/hamburgermenu';
+
+const UserProfile = () => {
+    return (
+        <div>
+           <div> <BackArrow /></div>
+            <div><Hamburgermenu /></div>
+        </div>
+    );
+};
+
+export default UserProfile;
