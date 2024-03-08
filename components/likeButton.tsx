c424ac68871ace525b45411bbc98cfc286915336
svanlaugbirna <svanlaugbirna@gmail.com>
2024-03-08 09:15:29 +0000

    function on the liked news/posted news button active

---
+++ components/likeButton.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+type Props = {
+    title:string
+    handleClick: () => void;
+    isActive: boolean;
+};
+
+const LikeButton: React.FC<Props> = ({title, handleClick, isActive}) => {
+
+    
+    return (
+        <div>
+            <button className={ `rounded-md h-12 ${isActive? "bg-darkGreen text-white": "bg-notWhite"} w-[165px] `} onClick={handleClick}>{title}</button>
+        </div>
+    );
+};
+
+export default LikeButton;
