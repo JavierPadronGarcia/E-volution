48c2fc3f5128fdd441a568bcc99b52cd695dd282
svanlaugbirna <svanlaugbirna@gmail.com>
2024-03-08 11:49:12 +0000

    fonts not working

---
+++ app/calculatePage/page.tsx
@@ -0,0 +1,18 @@
+import BackArrow from "@/components/backArrow";
+
+const CalculatePage = () => {
+    return (
+        <div>
+            <div className="block"><BackArrow/></div>
+            <div className=" flex flex-col items-center justify-center gap-3">
+                <p className="mt-[140px]"> Your renewable energies</p>
+                <div className="bg-notWhite w-[148px] h-[148px] rounded-xl"></div>
+            </div>
+            <div> 
+                <p className="h1page ">Calculate your renewable energy</p>
+            </div>
+        </div>
+    )
+}
+
+export default CalculatePage;
