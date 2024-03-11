c7aa71747c81d79bb76d7b577301b4c460d74e19
svanlaugbirna <svanlaugbirna@gmail.com>
2024-03-11 13:00:50 +0000

    the styling is hopefully complete, fonts still not working for me

--- app/calculatePage/page.tsx
+++ app/calculatePage/page.tsx
@@ -10,43 +10,43 @@ const CalculatePage = () => {
 
 {/**content bg- p1 m0 and h-full */}
             <div className="block"><BackArrow/></div>
-            <div className=" flex flex-col items-center justify-center gap-3">
-                <p className="mt-[140px]"> Your renewable energies</p>
-                <div className="bg-notWhite w-[148px] h-[148px] rounded-xl"></div>
-            </div>
-            <div> 
-                <p className="font-bold text-[20px]">Calculate your renewable energy</p>
-            </div>
-            <div className="justify-center flex pt-[150px]">
-     
-<div className="justify-center items-center">
-{/*here's the input for Total energy consumption*/}
-            <form className="animate-in flex-1 flex flex-col w-full justify-center  text-foreground">
-            <label className="text-md" htmlFor="text"></label>
-          <input
-            className="rounded-xl px-4 py-2 bg-inherit border-2 border-darkGreen mb-8"
-            name="energy consumption"
-            placeholder="Total energy consumption"
-            required/></form>
+                <div className=" flex flex-col items-center justify-center gap-3">
+                    <p className="mt-[140px]"> Your renewable energies</p>
+                    <div className="bg-notWhite w-[148px] h-[148px] rounded-xl"></div>
+                    </div>
+                        <div className="justify-center items-center m-14"> 
+                            <p className="font-bold text-[20px] text-center pt-5">Calculate your renewable energy</p>
+                        </div>
 
-          {/* Here's the input for Renewable energy
-          prufa komment*/ }
-          <form className="animate-in flex-1 flex flex-col w-full justify-center  text-foreground">
-            <label className="text-md" htmlFor="text"></label>
-          <input
-            className="rounded-xl px-4 py-2 bg-inherit border-2 border-darkGreen mb-8 w-[259px]"
-            name="renewable"
-            placeholder="Renewable energy"
-            required/>
 
-            <SubmitButton 
-            pendingText="Calculating...">
-              Calculate
-            </SubmitButton>
+            
+        {/*here's the input for Total energy consumption*/}
+            <div className="justify-center items-center ">
+
+            <form className="animate-in flex-1 flex flex-col w-full justify-center items-center  text-foreground gap-9">
+                <label className="text-md w-[259px]" htmlFor="text">
+                <input className="rounded-xl px-4 py-2 bg-inherit border-2 border-darkGreen w-full"
+                    name="energy consumption"
+                    placeholder="Total energy consumption"
+                    required/></label>
+
+                {/* Here's the input for Renewable energy*/ }
+                <label className="text-md w-[259px]" htmlFor="text">
+                <input
+                    className="rounded-xl px-4 py-2 bg-inherit border-2  border-darkGreen  w-full"
+                    name="renewable"
+                    placeholder="Renewable energy"
+                    required/>
+                    </label>
+
+                <SubmitButton 
+                    pendingText="Calculating...">
+                    Calculate
+                </SubmitButton>
             </form>
           </div>
           </div>
-        </div>
+      
     );
 };
 
