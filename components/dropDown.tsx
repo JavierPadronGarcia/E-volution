f6a04e2f77ef8b03d05b2ba8cac1cc9ba4e8c1e9
svanlaugbirna <svanlaugbirna@gmail.com>
2024-03-12 13:43:35 +0000

    almost ready

--- components/dropDown.tsx
+++ components/dropDown.tsx
@@ -3,8 +3,8 @@ import { AiOutlineLogout } from "react-icons/ai";
 
 const DropDown = () => {
   return (
-    <div className="a">
-      <div className="bg-white rounded-md w-[150px] h-[70px] flex flex-col justify-center">
+    <div className="absolute right-5 top-[80px] z-10">
+      <div className="bg-notWhite border-darkGreen border-2 rounded-md w-[150px] h-[75px] flex flex-col justify-center gap-2 p-4">
         <div className=" w-[122] h-[70px] flex flex-row gap-2">
           <FiEdit3 className="w-[22px] h-[22px]" /> <p>Edit profile</p>
         </div>
