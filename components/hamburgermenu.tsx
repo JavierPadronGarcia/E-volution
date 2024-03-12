f6c9770d5a7b9dde009150a6596d782d8adae7d9
svanlaugbirna <svanlaugbirna@gmail.com>
2024-03-12 20:54:06 +0000

    dropdownmenu is ready

--- components/hamburgermenu.tsx
+++ components/hamburgermenu.tsx
@@ -1,24 +1,26 @@
-'use client'
-import React, {useState} from "react";
+"use client";
+import React, { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import DropDown from "./dropDown";
-
+import { AiOutlineClose } from "react-icons/ai";
 
 const Hamburgermenu = () => {
     const [isOpen, setIsOpen] = useState(false);
     const handleClick = () => {
         setIsOpen(!isOpen);
+
     } 
 
     return (
-     <div>
-        <RxHamburgerMenu onClick={handleClick} className="h-[32px] w-[32px] absolute top-10 right-10"></RxHamburgerMenu>
-        {isOpen && 
         <div>
-        
-            <DropDown></DropDown>
-        </div>}
-     </div>
- ) }
-  
-  export default Hamburgermenu;
+           {isOpen? <AiOutlineClose onClick={handleClick} className="h-[32px] w-[32px] absolute top-10 right-10"></AiOutlineClose>:
+           <RxHamburgerMenu onClick={handleClick} className="h-[32px] w-[32px] absolute top-10 right-10"></RxHamburgerMenu>}
+           {isOpen && 
+           <div>
+           
+               <DropDown></DropDown>
+           </div>}
+        </div>
+    ) }
+     
+     export default Hamburgermenu;
