cece4d56a94ccffa3f8b88b2303a79e2f84ad6d0
svanlaugbirna <svanlaugbirna@gmail.com>
2024-03-13 08:49:27 +0000

    added login and register

---
+++ app/selectedNews/page.tsx
@@ -0,0 +1,33 @@
+import BackArrow from "@/components/backArrow";
+import Link from "next/link";
+
+const SelectedNews = () => {
+
+    return (
+        <>
+        <div>
+            <BackArrow/>
+            <div className="flex flex-col font-bold text-[20px] justify-center text-center place-content-center mt-[80px] mb-5"><h1>Title of the news/article</h1></div>
+        <div className="border-2 border-darkGreen h-[206px] w-[325px]">
+            <img src="/breakingNews.png" className="size-full"></img>
+        </div>
+            
+        </div>
+        <div className="text-center justify-center p-8 ">
+        <p>
+            Renewable energycomes from unlimited, naturally replenished resources, such as the sun, tides, and wind. Renewable energy can be used for electricity generation, space and water heating and cooling, and transportation. Non-renewable energy, in contrast, comes from finite sources, such as coal, natural gas, and oil.
+            Renewable energycomes from unlimited, naturally replenished resources, such as the sun, tides, and wind. Renewable energy can be used for electricity generation, space and water heating and cooling, and transportation. Non-renewable energy, in contrast, comes from finite sources, such as coal, natural gas, and oil.
+            Renewable energycomes from unlimited, naturally replenished resources, such as the sun, tides, and wind. Renewable energy can be used for electricity generation, space and water heating and cooling, and transportation. Non-renewable energy, in contrast, comes from finite sources, such as coal, natural gas, and oil.
+            Renewable energycomes from unlimited, naturally replenished resources, such as the sun, tides, and wind. Renewable energy can be used for electricity generation, space and water heating and cooling, and transportation. Non-renewable energy, in contrast, comes from finite sources, such as coal, natural gas, and oil.
+         </p>
+        </div>
+        <div className="flex gap-7 justify-center w-full m-5">
+            <Link href='/login' className="underline justify-center flex">Login</Link>
+            <Link href='/register' className="underline justify-center flex">Register</Link>
+          </div>
+        </>
+
+    )
+}
+
+export default SelectedNews
