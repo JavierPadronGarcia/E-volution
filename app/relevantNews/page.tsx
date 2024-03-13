8c00d4af6ccb59ac0587039406903bde09f53a59
NicolaiSzuwalski <132432642+NicolaiSzuwalski@users.noreply.github.com>
2024-03-13 11:48:23 +0000

    author

--- app/relevantNews/page.tsx
+++ app/relevantNews/page.tsx
@@ -2,43 +2,41 @@ import React from 'react';
 import Article from '@/components/article';
 import BackArrow from '@/components/backArrow';
 import DownRightArrow from '@/components/downRightArrow';
+import Author from '@/components/author';
 
 const MyPage: React.FC = () => {
   return (
     <>
-    <div className='grid gap-8 grid-cols-1'>
-      <div className='relative'>
-        <Article
-          image='/breakingNews.png'
-          title="Put title here"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <div className='absolute right-0 bottom-0'>
-          <DownRightArrow></DownRightArrow>
-        </div>
-      </div>
+      <div className='grid gap-8 grid-cols-1'>
 
-      <div className='relative'>
-        <Article
-          image='/breakingNews.png'
-          title="Put title here"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <div className='absolute right-0 bottom-0'>
-          <DownRightArrow></DownRightArrow>
+        <div className='relative'>
+          <Article
+            image='/breakingNews.png'
+            title="Put title here"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
         </div>
-      </div>
 
-      <div className='relative'>
-        <Article
-          image='/breakingNews.png'
-          title="Put title here"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <div className='absolute right-0 bottom-0'>
-          <DownRightArrow></DownRightArrow>
+        <div className='relative'>
+          <Article
+            image='/breakingNews.png'
+            title="Put title here"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
         </div>
+
+        <div className='relative'>
+          <Article
+            image='/breakingNews.png'
+            title="Put title here"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+        </div>
+
       </div>
+      <div>
+        Made by
+        <Author></Author>
       </div>
     </>
   );
---
+++ components/author.tsx
@@ -0,0 +1,14 @@
+import { CgProfile } from "react-icons/cg";
+
+
+const Author = () => {
+    return(
+            
+            <div className="border-2 h-[45px] flex flex-row items-center rounded-xl w-auto item-center px-4 py-6 ">
+            <CgProfile className="w-[33px] h-[33px] mr-2"/> 
+            Svanlaug
+            </div>
+    )
+}
+
+export default Author;
